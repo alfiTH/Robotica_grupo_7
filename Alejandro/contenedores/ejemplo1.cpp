f234f6d93757e93962c7cb2a59241373c3139b73
alfiTH <alex_alfi@hotmail.com>
2022-09-20 19:04:34 +0200

    parallel code contenedores

--- Alejandro/contenedores/ejemplo1.cpp
+++ Alejandro/contenedores/ejemplo1.cpp
@@ -6,17 +6,36 @@ Contenedores::Contenedores()
     std::mt19937 rng(dev());
     std::uniform_int_distribution<std::mt19937::result_type> dist6(1,1000000); // distribution in range [1, 6]
 	vec.resize(1000000000);
+	auto t1 = std::chrono::high_resolution_clock::now();
 	for(auto &n:vec)
 	{
 		n=dist6(rng);
 	}
+	auto t2 = std::chrono::high_resolution_clock::now();
+	qInfo() << "Inicializacion de vector" << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
 
-	auto t1 = std::chrono::high_resolution_clock::now();
+
+	vec2.resize(1000000000);
+	t1 = std::chrono::high_resolution_clock::now();
+	#pragma omp parallel for
+	for(auto &n:vec2)
+	{
+		n=dist6(rng);
+	}
+	t2 = std::chrono::high_resolution_clock::now();
+	qInfo() << "Inicializacion de vector OMP" << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+
+	t1 = std::chrono::high_resolution_clock::now();
 	std::ranges::sort(vec);
-    auto t2 = std::chrono::high_resolution_clock::now();
-	qInfo()<< std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
-
+    t2 = std::chrono::high_resolution_clock::now();
+	qInfo() << "Sort de vector" << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
 	
+	t1 = std::chrono::high_resolution_clock::now();
+    #pragma omp parallel 
+	std::ranges::sort(vec);
+	t2 = std::chrono::high_resolution_clock::now();
+	qInfo() << "Sort OMP de vector" << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+
 
 }
 
--- Alejandro/contenedores/ejemplo1.h
+++ Alejandro/contenedores/ejemplo1.h
@@ -6,11 +6,13 @@
 #include <QDebug>
 #include <ranges>
 #include <chrono>
+#include <omp.h>
 
 class Contenedores
 {
     private:
         std::vector<int> vec;
+        std::vector<int> vec2;
     public:
         Contenedores();
         void sort_vector();
